22d2904af0c0cc2b506ebfedd4c6123c87236104
Morteza Moallemi <41530553+mortezamoallemi@users.noreply.github.com>
2023-04-13 21:27:54 +1000

    update

--- modules/home.R
+++ modules/home.R
@@ -7,7 +7,7 @@ UI <- function(id = pagename) {
               h2("Dynamic ShinyApp Example"),
                 p("This is an example of a dynamic ShinyApp where the pages areread from an external source, e.g. github repository. The benefit of this dynamicity is there is no need to deploy the app after each update, rather any update to the source file is refleted in 'shinyapps.io' just by refreshing the page."),
               p("The code for this app can be accessed here:"),
-              tags$a(href="https://github.com/mortezamoallemi/dynamic_shinyapp_example")
+              tags$a(href="https://github.com/mortezamoallemi/dynamic_shinyapp_example", "App Github Repository")
               )
   )
 }
